1602309f3e59228987c0d679be6fbf6c4da0beed
Mlouise99 <106527744+Mlouise99@users.noreply.github.com>
2025-03-26 12:13:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // typing animation
 var typed = new Typed(".typing",{
-    strings:["Front-End developer","Network Engineer ","Video Editor"],
+    strings:["Front-End developer","Network Engineer ","IT Support"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true, 
